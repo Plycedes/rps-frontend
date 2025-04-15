7e74261096b085fc6788969dfb5effd824d92098
Plycedes <abhaypratapyadav340@gmail.com>
2025-04-15 11:01:47 +0530

    Project Setup

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Game from "./Game";
+import Leaderboard from "./Leaderboard";
+import Marketplace from "./Marketplace";
+import Profile from "./Profile";
+import Sidebar from "./Sidebar";
+import Tournaments from "./Tournaments";
+
+export { Game, Leaderboard, Marketplace, Profile, Sidebar, Tournaments };
