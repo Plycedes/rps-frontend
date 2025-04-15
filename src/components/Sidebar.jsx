fae62d6b58a156902aa15422e694920e57d76d1a
Plycedes <abhaypratapyadav340@gmail.com>
2025-04-15 16:40:50 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useAxios } from "../hooks/useAxios";
+import { logoutUser } from "../utils/api";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/authSlice";
 
 function Sidebar() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.auth.user);
+    const { loading, error, fetchData } = useAxios();
 
-    //[#414264]
+    const handleLogout = async () => {
+        const res = await fetchData(() => logoutUser());
+        if (res?.statusCode === 200) {
+            dispatch(setUser({ user: "", token: "" }));
+            navigate("/");
+        }
+    };
     return (
         <div className="flex">
             <div className="w-64 bg-transparent text-white h-screen p-4">
@@ -18,8 +32,13 @@ function Sidebar() {
                             C<b className="text-primary">O</b>RE
                         </h1>
                     </div>
-                    <div className="rounded bg-gray-800 p-2 py-2 mt-5">
-                        {/* User name and profile image */}
+                    <div className="rounded bg-gray-800 p-2 px-3 mt-5">
+                        {user && (
+                            <div className="flex items-center gap-3">
+                                <img src={user.avatar} className="w-8 h-8 rounded-full" />
+                                <p className="text-xl text-purple-500 pb-1">{user.username}</p>
+                            </div>
+                        )}
                     </div>
                 </div>
                 <div className="h-5/6 pb-5 pt-1">
@@ -30,7 +49,7 @@ function Sidebar() {
                                     <div className="flex gap-2 items-center block py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
                                             src="https://img.icons8.com/?size=100&id=GFDbOB2OqPWt&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            className="w-7 h-7"
                                         />
                                         Play
                                     </div>
@@ -40,8 +59,8 @@ function Sidebar() {
                                 <NavLink to="tournaments">
                                     <div className="flex gap-2 items-center block py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
-                                            src="https://img.icons8.com/?size=100&id=uSWplRVhqqlS&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            src="https://img.icons8.com/?size=100&id=NuFBBv7mvFEA&format=png&color=7726e6"
+                                            className="w-7 h-7"
                                         />
                                         Tournaments
                                     </div>
@@ -52,7 +71,7 @@ function Sidebar() {
                                     <div className="flex gap-2 items-center block py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
                                             src="https://img.icons8.com/?size=100&id=Ib9FADThtmSf&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            className="w-7 h-7"
                                         />
                                         Profile
                                     </div>
@@ -63,7 +82,7 @@ function Sidebar() {
                                     <div className="flex gap-2 items-center block py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
                                             src="https://img.icons8.com/?size=100&id=70624&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            className="w-7 h-7"
                                         />
                                         Leaderboard
                                     </div>
@@ -73,8 +92,8 @@ function Sidebar() {
                                 <NavLink to="marketplace">
                                     <div className="flex gap-2 items-center block pt-3 py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
-                                            src="https://img.icons8.com/?size=100&id=7775&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            src="https://img.icons8.com/?size=100&id=78230&format=png&color=7726e6"
+                                            className="w-7 h-7"
                                         />
                                         Marketplace
                                     </div>
@@ -85,7 +104,7 @@ function Sidebar() {
                                     <div className="flex gap-2 items-center block pt-3 py-2 px-4 hover:bg-gray-700 rounded">
                                         <img
                                             src="https://img.icons8.com/?size=100&id=2848&format=png&color=7726e6"
-                                            className="w-8 h-6"
+                                            className="w-7 h-7"
                                         />
                                         Contact Us
                                     </div>
@@ -105,14 +124,27 @@ function Sidebar() {
                                         className="flex items-center justify-center gap-1 bg-primary mt-3 w-full text-white py-2 rounded hover:bg-purple-800 focus:outline-none text-xl"
                                         onClick={() => {}}
                                     >
-                                        Search
                                         <img
-                                            src="https://img.icons8.com/?size=100&id=7789&format=png&color=FFFFFF"
-                                            className="w-8 h-6 mt-1"
+                                            src="https://img.icons8.com/?size=100&id=7695&format=png&color=FFFFFF"
+                                            className="w-7 h-7 mt-1"
                                         />
+                                        Search
                                     </button>
                                 </div>
                             </li>
+                            <li>
+                                <hr className="my-3 border-t-2 border-gray-400" />
+                            </li>
+                            <button
+                                className="flex items-center justify-center gap-1 bg-gray-600 mt-3 w-full text-white py-2 rounded hover:bg-purple-800 focus:outline-none text-xl"
+                                onClick={handleLogout}
+                            >
+                                <img
+                                    src="https://img.icons8.com/?size=100&id=26217&format=png&color=FFFFFF"
+                                    className="w-7 h-7 mt-1"
+                                />
+                                {loading ? "Logging out..." : "Logout"}
+                            </button>
                         </ul>
                     </div>
                 </div>
