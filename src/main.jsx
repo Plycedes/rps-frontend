90d28d87878699544f0dde111513f640639db010
Plycedes <abhaypratapyadav340@gmail.com>
2025-04-15 12:32:08 +0530

    Auth and Socket slice

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
     Route,
 } from "react-router-dom";
 import { Game, Leaderboard, Marketplace, Profile, Tournaments } from "./components";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -22,8 +24,10 @@ const router = createBrowserRouter(
 );
 
 createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router}>
-        <App />
-    </RouterProvider>
+    <Provider store={store}>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </Provider>
 );
 
