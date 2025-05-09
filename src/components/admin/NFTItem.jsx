00bc59f915ad7d5b633cfa26415e4d0fbee581af
Plycedes <abhaypratapyadav340@gmail.com>
2025-05-09 18:23:10 +0530

    NFT Item

---
+++ src/components/admin/NFTItem.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Copy } from "lucide-react";
+
+export const NFTItem = ({ nft }) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = async (nftId) => {
+        try {
+            await navigator.clipboard.writeText(nftId);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        } catch (err) {
+            console.error("Failed to copy text:", err);
+        }
+    };
+    return (
+        <div className="flex gap-4">
+            <img src={nft.imageUrl} className="w-25 h-25 rounded-lg border border-purple-500 p-1" />
+            <div className="flex flex-col gap-4">
+                <h2 className="text-xl font-semibold text-purple-400">{nft.name}</h2>
+                <div className="flex items-center justify-between bg-gray-800 text-white p-3 rounded border border-gray-600 w-fit max-w-full">
+                    <span className="truncate">{nft._id}</span>
+                    <button
+                        onClick={() => handleCopy(nft._id)}
+                        className="ml-3 hover:text-purple-400"
+                    >
+                        {copied ? (
+                            <img
+                                src="https://img.icons8.com/?size=100&id=3061&format=png&color=ad46ff"
+                                className="w-5 h-5"
+                            />
+                        ) : (
+                            <Copy size={18} />
+                        )}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/components/admin/ViewNFTs.jsx
+++ src/components/admin/ViewNFTs.jsx
@@ -2,13 +2,11 @@ import { useAxios } from "../../hooks/useAxios.js";
 import { useEffect, useState } from "react";
 import { getUserNFTs } from "../../utils/api";
 import Loader from "../Loader";
-import { Copy } from "lucide-react";
+import { NFTItem } from "./NFTItem.jsx";
 
 function ViewNFTs() {
     const { loading, error, fetchData } = useAxios();
     const [adminNFT, setAdminNFT] = useState([]);
-    const [copied, setCopied] = useState(false);
-    const textToCopy = "0xAbCdEf1234567890";
 
     useEffect(() => {
         (async () => {
@@ -17,18 +15,8 @@ function ViewNFTs() {
         })();
     }, []);
 
-    const handleCopy = async () => {
-        try {
-            await navigator.clipboard.writeText(textToCopy);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
-        } catch (err) {
-            console.error("Failed to copy text:", err);
-        }
-    };
-
     return (
-        <div className="p-2 w-full h-full">
+        <div className="px-2 py-4 w-full h-full">
             <div
                 className="space-y-4 overflow-y-auto scrollbar-thin scrollbar-thumb-purple-800 scrollbar-track-transparent w-full h-full
             border border-purple-500 rounded-lg p-4"
@@ -40,29 +28,7 @@ function ViewNFTs() {
                         key={nft._id}
                         className="bg-gray-800 p-4 shadow-md flex justify-between rounded-lg items-center"
                     >
-                        <div className="flex gap-4">
-                            <img
-                                src={nft.imageUrl}
-                                className="w-25 h-25 rounded-lg border border-purple-500 p-1"
-                            />
-                            <div className="flex flex-col gap-4">
-                                <h2 className="text-xl font-semibold text-purple-400">
-                                    {nft.name}
-                                </h2>
-                                <div className="flex items-center justify-between bg-gray-800 text-white p-3 rounded border border-gray-600 w-fit max-w-full">
-                                    <span className="truncate">{textToCopy}</span>
-                                    <button
-                                        onClick={handleCopy}
-                                        className="ml-3 hover:text-purple-400"
-                                    >
-                                        <Copy size={18} />
-                                    </button>
-                                    {copied && (
-                                        <span className="ml-2 text-green-400 text-sm">Copied!</span>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
+                        <NFTItem nft={nft} />
                     </p>
                 ))}
             </div>
